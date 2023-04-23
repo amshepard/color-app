11f957b60d0111647034f7aacabf20a76dcad167
amshepard <antoineshepard@pursuit.org>
2023-04-23 12:05:43 -0400

    Class activity - color app.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+const colors = [
+  "papayawhip",
+  "thistle",
+  "peachpuff",
+  "lightgoldenrodyellow",
+  "paleturquoise",
+];
 function App() {
+  const [color, setColor] = useState("azure");
+  const [counter, setCounter] = useState(0);
+  const [defaultFontColor, setFontColor] = useState(true);
+  function changeColor() {
+    const chooseIndex = Math.floor(Math.random() * colors.length);
+    setColor(colors[chooseIndex]);
+    setCounter(counter + 1);
+  }
+  function resetCounter() {
+    setCounter(0);
+  }
+  function toggleFontColor() {
+    setFontColor(!defaultFontColor);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh", backgroundColor: color }}>
+      <button onClick={changeColor}>Change color</button>
+      <h2>{color}</h2>
+      <h3 style={defaultFontColor ? { color: "black" } : { color: "white" }}>
+        The color has been changed {counter} times
+      </h3>
+      <button onClick={resetCounter}>Reset counter</button>
+      <button onClick={toggleFontColor}>Toggle font color</button>
     </div>
   );
 }
